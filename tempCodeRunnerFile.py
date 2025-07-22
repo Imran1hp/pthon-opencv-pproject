a8d637db3d3698f7f50b8fe5fa68af713fc7a590
ali imran <aliimranlaskar123@gmail.com>
2025-07-22 23:25:56 +0530

    drawing in a blank window

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-capture = cv.VideoCapture("Videos/dog.mp4")
-while True:
-    isTrue,frame =capture.read()
-    rescaleFrame = rescaleFrame(frame)
-    cv.imshow("Video",rescaleFrame)
-    if cv.waitKey(20) & 0xFF == ord('d'):
-        break
-capture.release()
-cv.destroyAllWindows()
+cv.waitKey(0)
